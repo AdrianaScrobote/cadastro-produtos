e50dda05d6c66850045386ebced44a2c15143e26
AdrianaScrobote <adriana_100000@gmail.com>
2020-07-04 20:51:52 -0300

    Adição das views home e de cadastro de produtos. Criação de rotas.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
+import Navbar from './components/navbar'
+import Rotas from './rotas'
+
+
+
 function App() {
   return (
-    <div className="alert alert-dismissible alert-warning">
-      <button type="button" className="close" data-dismiss="alert">&times;</button>
-      <h4 className="alert-heading">Warning!</h4>
-      <p className="mb-0">Hello Adriana, <a href="#" className="alert-link">Bootswatchhhh</a>.</p>
-    </div>
+    <>
+      <div className="container">
+        <Navbar/>
+        <Rotas/>
+      </div>
+    </>
   );
 }
 
